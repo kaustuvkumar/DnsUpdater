504539c356ce4e0a14cfc04161639e89c4cc8f11
Kaustuv <kaustuv.kumar12@gmail.com>
2022-04-22 13:56:20 +0530

    updated changes

--- examples/exercise/variables.tf
+++ examples/exercise/variables.tf
@@ -1,3 +1,7 @@
 # ----------------------------------------
 # Write your Terraform module inputs here
 # ----------------------------------------
+variable "file_name" {
+  description = "The name to use for all the input files"
+  type        = string
+}
